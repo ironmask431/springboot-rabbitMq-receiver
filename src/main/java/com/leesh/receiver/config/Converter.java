6ccd5942c5c38b87616352837cb32e1a78ee0c3d
ironmask431 <ironmask431@gmail.com>
2022-04-29 07:03:38 +0900

    rebbitmq receiver 객체 dto

--- src/main/java/com/leesh/receiver/config/Converter.java
+++ src/main/java/com/leesh/receiver/config/Converter.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class Converter {
+    //rabbit mq로 객체를 전송,수신 하기 위해서 필요한 컨버터 (객체를 byte[]로 직렬화 / String 과 byte[] 만 전송가능)
     @Bean
     public Jackson2JsonMessageConverter jsonMessageConverter(){
         return new Jackson2JsonMessageConverter();
--- src/main/java/com/leesh/receiver/module/ReceiverModule.java
+++ src/main/java/com/leesh/receiver/module/ReceiverModule.java
@@ -12,22 +12,25 @@ import org.springframework.stereotype.Component;
 @Component
 public class ReceiverModule {
 
+    //text 메세지 receiver
     @RabbitListener(bindings = @QueueBinding(
             exchange = @Exchange(name="time", type = ExchangeTypes.TOPIC), //sender 에서보낸 exchange 값
-            value = @Queue(name="time-second"),
+            value = @Queue(name="time-second"), //현재 receiver의 value 임의 설정
             key = "time-first" //sender 에서 보낸 routing key 값
     ))
     public void receiver1(String msg){
         System.out.println("<== [수신]receiver time : "+ msg);
     }
 
+
+    //DeptDto 객체 receiver
     @RabbitListener(bindings = @QueueBinding(
             exchange = @Exchange(name="deptDto", type = ExchangeTypes.TOPIC), //sender 에서보낸 exchange 값
-            value = @Queue(name="deptDto-2"),
+            value = @Queue(name="deptDto-2"), //현재 receiver의 value 임의 설정
             key = "deptDto-1" //sender 에서 보낸 routing key 값
     ))
     @SendTo("deptDto/deptDto-3") //수신받은 후 다시 리턴전송하기
-    //exchange는 deptDto, key는 deptDto-third 를 의미함.
+    //deptDto 는 exchange , deptDto-third 는 key는  를 의미함.
     public DeptDto receiver2(DeptDto deptDto){
         System.out.println("<== 2.[수신]receiver deptDto : "+ deptDto.toString());
         deptDto.setDeptNo(2);
